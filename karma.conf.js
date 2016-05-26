90bc219aee54fe355fc00eb6cfaaaa23c702d5d8
Lucien Greathouse <me@lpghatguy.com>
2016-05-25 21:54:27 -0600

    Add karma-mocha-reporter for better test logging in karma

--- karma.conf.js
+++ karma.conf.js
@@ -2,27 +2,22 @@
 
 module.exports = config => {
 	config.set({
-		frameworks: ["mocha"],
-
 		files: [
-		  "es5/test.js"
+			"es5/test.js"
 		],
 
-		reporters: ["progress"],
+		frameworks: ["mocha"],
+		reporters: ["mocha"],
+		browsers: ["Chrome"],
 
 		port: 9876,
-
 		colors: true,
 
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
 
 		autoWatch: true,
-
-		browsers: ["Chrome"],
-
 		singleRun: true,
-
 		concurrency: Infinity
 	});
 };
